bf7f1ab563dd1eac126c59243d82d0d906f97dfd
dythu <abelazady@foxmail.com>
2023-04-26 20:16:06 +0800

    1

--- .history/main_20230413102350.py
+++
@@ -1,9 +0,0 @@
-import math
-import torch.nn as nn
-def func(x, y):
-    if x<y:
-        y = math.sin(x)
-    else:
-        y = math.cos(x)
-    return y
-
--- .history/snn_20230413094316.py
+++
@@ -1,6 +0,0 @@
-import math
-def func(x, y):
-    if x<y:
-        y = math.sin(x)
-    else:
-        y = math.cos(x)
